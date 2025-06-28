332cbfec8c19c6c713abe59f56fb4299fd60c6da
David Lightfoot <dslightfoot@yahoo.com>
2025-06-27 20:08:33 -0400

    Update UI

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,8 +1,8 @@
-import { drizzle } from "drizzle-orm/libsql";
+// import { drizzle } from "drizzle-orm/libsql";
 import { createPool, type Pool } from "mysql2/promise";
 
 import { env } from "~/env";
-import * as schema from "./schema";
+// import * as schema from "./schema";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
