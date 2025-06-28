d8f9fa1cc501f6d0cb418a2e11d836120ec152e4
David Lightfoot <dslightfoot@yahoo.com>
2025-06-27 20:25:39 -0400

    Update UI

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,14 +4,9 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "singlestore",
-  tablesFilter: ["drive-tutorial_*"],
+  dialect: "sqlite",
   dbCredentials: {
-    host: env.SINGLESTORE_HOST,
-    port: parseInt(env.SINGLESTORE_PORT),
-    user: env.SINGLESTORE_USER,
-    password: env.SINGLESTORE_PASS,
-    database: env.SINGLESTORE_DB_NAME,
-    ssl: {},
+    url: env.DATABASE_URL,
   },
+  tablesFilter: ["drive-tutorial_*"],
 } satisfies Config;
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,30 +1,19 @@
-// import { drizzle } from "drizzle-orm/libsql";
-import { createPool, type Pool } from "mysql2/promise";
+import { createClient, type Client } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
 
 import { env } from "~/env";
-// import * as schema from "./schema";
+import * as schema from "./schema";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  conn: Pool | undefined;
+  client: Client | undefined;
 };
 
-const conn =
-    globalForDb.conn ??
-    createPool({
-      host: env.SINGLESTORE_HOST,
-      port: parseInt(env.SINGLESTORE_PORT),
-      user: env.SINGLESTORE_USER,
-      password: env.SINGLESTORE_PASS,
-      database: env.SINGLESTORE_DB_NAME,
-      ssl: {},
-      maxIdle: 0,
-    });
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+export const client =
+  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
+if (env.NODE_ENV !== "production") globalForDb.client = client;
 
-conn.addListener("error", (err) => {
-  console.error("Database connection error", err);
-});
+export const db = drizzle(client, { schema });
